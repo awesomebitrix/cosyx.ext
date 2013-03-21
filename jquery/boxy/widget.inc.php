c7f4c96c60f231cbf8f0204062964c4d7c77dd7d
Aleksey Peredelskiy <info@web-n-roll.ru>
2013-03-21 14:37:53 +0400

    f boxy for cp1251

--- jquery/boxy/widget.inc.php
+++ jquery/boxy/widget.inc.php
@@ -7,5 +7,9 @@ class JQuery_Boxy_Widget extends CSX_SingletonWidget {
 
 		$this->application->AddHeadScript($this->getPubUrl() . '/js/jquery.boxy.js');
 		$this->application->AddHeadStylesheet($this->getPubUrl() . '/css/boxy.css');
+
+		if (LANG_CHARSET=='windows-1251') {
+			$this->application->AddHeadScript($this->getPubUrl() . '/js/jquery.boxy.1251.js');
+		}
 	}
 }
