0a4832e9a491de025a589db2fb4d8d9256899efb
Aleksey Peredelskiy <info@web-n-roll.ru>
2013-06-12 10:59:59 +0400

    + scrollpane

---
+++ jquery/scrollpane/widget.inc.php
@@ -0,0 +1,12 @@
+<?php
+class JQuery_ScrollPane_Widget extends CSX_SingletonWidget {
+	protected function init() {
+		parent::init();
+
+		CSX_Widget::includeWidget('JQuery');
+
+		$this->application->AddHeadScript($this->getPubUrl() . '/jquery.mousewheel.js');
+		$this->application->AddHeadScript($this->getPubUrl() . '/jquery.jscrollpane.min.js');
+		$this->application->AddHeadStylesheet($this->getPubUrl() . '/jquery.jscrollpane.css');
+	}
+}
