eb0374c48806d22adcb0d60fff21426c0c134ac7
Aleksey Peredelskiy <info@web-n-roll.ru>
2013-12-03 09:56:05 +0400

    f

--- jquery/fancybox/widget.inc.php
+++ jquery/fancybox/widget.inc.php
@@ -6,11 +6,11 @@ class JQuery_Fancybox_Widget extends CSX_SingletonWidget {
         CSX_Widget::includeWidget('JQuery');
 
         $this->application->AddHeadScript($this->getPubUrl() . '/jquery.fancybox.js?v=2.1.5');
-        $this->application->AddHeadScript($this->getPubUrl() . '/helpers/jquery.fancybox-buttons.css?v=1.0.5');
         $this->application->AddHeadScript($this->getPubUrl() . '/helpers/jquery.fancybox-buttons.js?v=1.0.5');
         $this->application->AddHeadScript($this->getPubUrl() . '/helpers/jquery.fancybox-thumbs.js?v=1.0.7');
         $this->application->AddHeadScript($this->getPubUrl() . '/helpers/jquery.fancybox-media.js?v=1.0.6');
 
+        $this->application->AddHeadStylesheet($this->getPubUrl() . '/helpers/jquery.fancybox-buttons.css?v=1.0.5');
         $this->application->AddHeadStylesheet($this->getPubUrl() . '/jquery.fancybox.css?v=2.1.5');
         $this->application->AddHeadStylesheet($this->getPubUrl() . '/helpers/jquery.fancybox-thumbs.css?v=1.0.7');
     }
