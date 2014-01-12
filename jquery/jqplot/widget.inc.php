1db45c173a927fd5b6f80537f5055e29238cc8f5
vsidorova <bubasta@gmail.com>
2014-01-12 19:59:34 +0100

    jqplot

---
+++ jquery/jqplot/widget.inc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Cosyx Bitrix Extender Project
+ *
+ * @package cosyx.ext
+ * @version $Id$
+ * @author Peredelskiy Aleksey <info@web-n-roll.ru>
+ */
+
+/**
+ *  @package cosyx.ext
+ */
+class JQuery_JQPlot_Widget extends CSX_SingletonWidget {
+    protected function init() {
+        parent::init();
+
+        CSX_Widget::includeWidget('JQuery');
+
+        $this->application->AddHeadScript($this->getPubUrl() . '/excanvas.min.js');
+        $this->application->AddHeadScript($this->getPubUrl() . '/jquery.jqplot.min.js');
+        $this->application->AddHeadStylesheet($this->getPubUrl() . '/jquery.jqplot.min.css');
+    }
+}
