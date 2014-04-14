db73ff380159a70206768ee6a6cee4cefcc595e8
vsidorova <bubasta@gmail.com>
2014-04-14 10:43:31 +0400

    f

--- jquery/jcarousel/widget.inc.php
+++ jquery/jcarousel/widget.inc.php
@@ -8,15 +8,14 @@
  */
 
 /**
- *	@package cosyx.ext
+ *  @package cosyx.ext
  */
 class JQuery_JCarousel_Widget extends CSX_SingletonWidget {
-	protected function init() {
-		parent::init();
+    protected function init() {
+        parent::init();
 
-		CSX_Widget::includeWidget('JQuery');
+        CSX_Widget::includeWidget('JQuery');
 
-		$this->application->AddHeadScript($this->getPubUrl() . '/lib/jquery.jcarousel.min.js');
-		$this->application->AddHeadString('<link href="' . $this->getPubUrl() . '/skins/tango/skin.css" type="text/css" rel="stylesheet" />', true);
-	}
+        $this->application->AddHeadScript($this->getPubUrl() . '/lib/jquery.jcarousel.min.js');
+    }
 }
