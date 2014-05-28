8af231544398fa7d3c453dbef3218e2f7e1efa2c
Aleksey Peredelskiy <info@web-n-roll.ru>
2014-05-28 16:06:03 +0400

    f

--- jquery/widget.inc.php
+++ jquery/widget.inc.php
@@ -16,11 +16,13 @@ class JQuery_Widget extends CSX_SingletonWidget {
     protected function init() {
         parent::init();
 
-        if ($this->params->has('v')) {
-            $this->application->AddHeadScript('http://code.jquery.com/jquery-' . $this->params->get('v') . '.min.js');
-        }
-        else {
-            $this->application->AddHeadScript($this->getPubUrl() . '/jquery-1.11.0.min.js');
+        if (!defined(CSX_CUSTOM_JQUERY)) {
+            if ($this->params->has('v')) {
+                $this->application->AddHeadScript('http://code.jquery.com/jquery-' . $this->params->get('v') . '.min.js');
+            }
+            else {
+                $this->application->AddHeadScript($this->getPubUrl() . '/jquery-1.11.0.min.js');
+            }
         }
     }
 }
