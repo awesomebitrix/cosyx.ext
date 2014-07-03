f4d8e1947fcbeb52563af0b11f623b65b8fac86e
Aleksey Peredelskiy <info@web-n-roll.ru>
2014-07-03 07:38:20 +0400

    jquery ui upgrade

--- jquery/ui/widget.inc.php
+++ jquery/ui/widget.inc.php
@@ -1,22 +1,24 @@
 <?php
 class JQuery_UI_Widget extends CSX_SingletonWidget {
-	protected function init() {
-		parent::init();
+    protected function init() {
+        parent::init();
 
-		CSX_Widget::includeWidget('JQuery');
+        CSX_Widget::includeWidget('JQuery');
 
-		$this->application->AddHeadScript($this->getPubUrl() . '/js/jquery-ui-1.10.3.custom.min.js');
+        $this->application->AddHeadScript($this->getPubUrl() . '/js/jquery-ui.min.js');
 
-		if (!defined('LANG_CHARSET') || LANG_CHARSET!='windows-1251') {
-			$this->application->AddHeadScript($this->getPubUrl() . '/js/i18n/jquery.ui.datepicker-ru.js');
-		}
-		else {
-			$this->application->AddHeadScript($this->getPubUrl() . '/js/i18n/jquery.ui.datepicker-ru1251.js');
-		}
+        if (!defined('LANG_CHARSET') || LANG_CHARSET!='windows-1251') {
+            $this->application->AddHeadScript($this->getPubUrl() . '/js/i18n/jquery.ui.datepicker-ru.js');
+        }
+        else {
+            $this->application->AddHeadScript($this->getPubUrl() . '/js/i18n/jquery.ui.datepicker-ru1251.js');
+        }
+        
+        $this->application->AddHeadStylesheet($this->getPubUrl() . '/css/jquery-ui.min.css');
+        $this->application->AddHeadStylesheet($this->getPubUrl() . '/css/jquery-ui.structure.min.css');
 
-
-		$theme = $this->params->get('theme');
-		if (!$theme) $theme = 'redmond';
-		$this->application->AddHeadStylesheet($this->getPubUrl() . '/themes/' . $theme . '/jquery-ui-1.8.12.custom.css');
-	}
+        $theme = $this->params->get('theme');
+        if (!$theme) $theme = 'redmond';
+        $this->application->AddHeadStylesheet($this->getPubUrl() . '/themes/' . $theme . '/jquery-ui.theme.min.css');
+    }
 }
